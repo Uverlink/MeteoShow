5bf619c19c5ff21e7e9cfe006241101ce6598cb2
Uverlink <marc.pizzanelli@gmail.com>
2022-10-28 10:26:25 +0200

    Navigation

--- App.js
+++ App.js
@@ -3,13 +3,40 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import About from './components/About';
 import Search from './components/Search'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+let news = undefined;
 
 export default class App extends Component {
   render () {
     return (
-      <View style={{marginVertical: 40}}>
-          <Search />
-     </View>
+      <NavigationContainer>
+      <Tab.Navigator
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+      
+                  if (route.name === 'Search') {
+                    iconName = focused
+                      ? 'partly-sunny'
+                      : 'partly-sunny-outline';
+                  } else if (route.name === 'About') {
+                    iconName = focused ? 'information-circle' : 'information-circle-outline';
+                  }
+      
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'black',
+                tabBarInactiveTintColor: 'gray',
+              })}>
+        <Tab.Screen name="Search" component={Search} options={{ tabBarBadge: news }} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator>
+    </NavigationContainer>
    );
   }
 }
