50323e5b903757d76058d2a8e4738c89808cdfe7
Uverlink <marc.pizzanelli@gmail.com>
2022-10-28 09:55:44 +0200

    Search Bar

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import About from './components/About';
+import Search from './components/Search'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>C'est instantané.</Text>
-      <About />
-      <StatusBar style="auto" />
-      <ActivityIndicator color='red' size='large' animating={true}/>
-    </View>
-  );
+export default class App extends Component {
+  render () {
+    return (
+      <View style={{marginVertical: 40}}>
+          <Search />
+     </View>
+   );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/Search.js
+++ components/Search.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 export default class Home extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            city: "Boulogne-sur-Mer"
+        }
+    }
+
+    setCity (city) {
+        this.setState({city})
+    }
+
     render() {
-        
+        return (
+            <TextInput
+            underlineColorAndroid='transparent' style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.setCity(text)}
+            value={this.state.city}
+            />
+        )
     }
 }
